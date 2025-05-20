a3a2a079746d61ed80abc40037bafdc9827a4fe2
Rikhil Nellimarla <rikhil.nellimarla@gmail.com>
2025-05-20 20:14:22 +0530

    cors fix
    Signed-off-by: Rikhil Nellimarla <rikhil.nellimarla@gmail.com>

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",  
-    "http://127.0.0.1:3000",  
+    "http://127.0.0.1:3000",
+    "https://voiceiqindominuslabs.vercel.app"
 ]
 
 app.add_middleware(
