7cf555bc8f72bc7d0372598cb1d2e0705483ef3e
Rikhil Nellimarla <rikhil.nellimarla@gmail.com>
2025-05-20 19:13:24 +0530

    fixed agents

--- agents.py
+++ agents.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
 from pydantic_ai import Agent
-from pydantic_ai.models.gemini import GeminiModelSettings, GeminiModel, GeminiModelName, LatestGeminiModelNames
+from pydantic_ai.models.groq import GroqModelSettings, GroqModel, GroqModelName
+from pydantic_ai.providers.groq import GroqProvider
+from pydantic_ai.models.gemini import GeminiModelSettings, GeminiModel, GeminiModelName
 from pydantic_ai.providers.google_vertex import GoogleVertexProvider
 from pydantic import BaseModel, Field
 from supabase import Client, create_client
@@ -11,31 +13,39 @@ from settings import Settings
 
 settings = Settings()
 
+# Groq Model Definition
+groq_settings = GroqModelSettings(
+    temperature=0.7,
+    top_p=0.95,
+    frequency_penalty=0,
+)
+
+groq_model_name: GroqModelName = "llama-3.3-70b-versatile"
+
 async_groq_client = groq.AsyncGroq(api_key=settings.groq_api_key)
 
+groq_model = GroqModel(
+    model_name=groq_model_name,
+    provider=GroqProvider(groq_client=async_groq_client),
+)
+
+# Gemini Model Definition
 gemini_settings = GeminiModelSettings(
     temperature=0.7,
     top_p=0.95,
     frequency_penalty=0,
 )
 
-provider = GoogleVertexProvider(
+gemini_provider = GoogleVertexProvider(
     service_account_info=settings.gcp_service_account_info,
     project_id=settings.gcp_project_id
 )
 
-report_model_name: GeminiModelName = "gemini-2.5-pro-preview-05-06"
+gemini_model_name: GeminiModelName = "gemini-2.5-pro-preview-05-06"
 
-general_model_name: GeminiModelName = "gemini-2.0-flash"
-
-report_model = GeminiModel(
-    model_name=report_model_name,
-    provider=provider
-)
-
-general_model = GeminiModel(
-    model_name=general_model_name,
-    provider=provider
+gemini_model = GeminiModel(
+    model_name=gemini_model_name,
+    provider=gemini_provider
 )
 
 
@@ -54,11 +64,11 @@ class Deps:
     
 deps = Deps()
 
-with open("prompts/report_agent_prompt.txt", "r") as file:
-    report_agent_prompt = file.read()
-
 with open("prompts/call_log_agent_prompt.txt", "r") as file:
     call_log_agent_prompt = file.read()
+
+with open("prompts/report_agent_prompt.txt", "r") as file:
+    report_agent_prompt = file.read()
 
 with open("prompts/database_agent_prompt.txt", "r") as file:
     database_agent_prompt = file.read()
@@ -66,31 +76,31 @@ with open("prompts/database_agent_prompt.txt", "r") as file:
 with open("prompts/chat_agent_prompt.txt", "r") as file:
     chat_agent_prompt = file.read()
 
+call_log_agent = Agent(
+    model=groq_model,
+    model_settings=groq_settings,
+    system_prompt=call_log_agent_prompt,
+    retries=3,
+)
+
 report_agent = Agent(
-    model=report_model,
+    model=gemini_model,
     model_settings=gemini_settings,
     system_prompt=report_agent_prompt,
     retries=3,
 )
 
-call_log_agent = Agent(
-    model=general_model,
-    model_settings=gemini_settings,
-    system_prompt=call_log_agent_prompt,
-    retries=3,
-)
-
 database_agent = Agent(
-    model=general_model,
-    model_settings=gemini_settings,
+    model=groq_model,
+    model_settings=groq_settings,
     system_prompt=database_agent_prompt,
     retries=3,
     output_type=Form
 )
 
 chat_agent = Agent(
-    model=general_model,
-    model_settings=gemini_settings,
+    model=groq_model,
+    model_settings=groq_settings,
     system_prompt=chat_agent_prompt,
     retries=3
 )
